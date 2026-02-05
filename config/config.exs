71c4e7b27e1ca20dab24f719c1d0b2fca7fff1e6
x <kemo@sent.com>
2026-02-04 21:55:01 -0600

    make envars optional for mix

--- config/config.exs
+++ config/config.exs
@@ -4,9 +4,9 @@ config :ex_aws, :http_client, ExAws.Request.Req
 
 config :ex_aws, :s3,
   scheme: "https://",
-  host: System.fetch_env!("KC_S3_HOST"),
-  access_key_id: System.fetch_env!("KC_S3_ACCESS_KEY"),
-  secret_access_key: System.fetch_env!("KC_S3_SECRET_KEY")
+  host: System.fetch_env("KC_S3_HOST"),
+  access_key_id: System.fetch_env("KC_S3_ACCESS_KEY"),
+  secret_access_key: System.fetch_env("KC_S3_SECRET_KEY")
 
 config :kc_core, KC.Repo,
   database: "kemoner_club",
